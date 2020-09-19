d53e4f3c94bb3fdf2096fe92b1dd7e4603bf2513
LQuangTien <tienkg5554@gmail.com>
2020-09-19 10:28:56 +0700

    Add: CRUD controller; Modify: kahoot.controller

--- src/modules/Kahoot/kahoot.model.ts
+++ src/modules/Kahoot/kahoot.model.ts
@@ -3,10 +3,15 @@ const Schema = mongoose.Schema;
 import Kahoot from './kahoot.interface';
 
 const kahootSchema = new mongoose.Schema({
-  userId: Schema.Types.ObjectId,
+  userId: {
+    type: Schema.Types.ObjectId,
+    get: toObjectId
+  },
   title: String
 });
-
+function toObjectId(userId: string) {
+  return mongoose.Types.ObjectId(userId);
+}
 const kahootModel = mongoose.model<Kahoot & mongoose.Document>(
   'Kahoot',
   kahootSchema,
---
+++ src/modules/crudController.ts
@@ -0,0 +1,104 @@
+import express from 'express';
+import status from 'http-status';
+import Response from '../helpers/response.helper';
+
+export default abstract class CrudController {
+  abstract model: any;
+  abstract controllerName: string;
+  abstract path: string;
+
+  getAll = async (req: express.Request, res: express.Response) => {
+    try {
+      const datas = await this.model.find().lean();
+      if (!datas.length) {
+        return Response(
+          res,
+          { message: `${this.controllerName} not found` },
+          status.NOT_FOUND
+        );
+      }
+    } catch (error) {
+      Response(res, { error: error }, status.INTERNAL_SERVER_ERROR);
+    }
+  };
+  getById = async (req: express.Request, res: express.Response) => {
+    try {
+      const { id } = req.params;
+      const data = await this.model.findById(id).lean();
+      if (!data) {
+        return Response(
+          res,
+          {
+            message: `${this.controllerName} not found`
+          },
+          status.NOT_FOUND
+        );
+      }
+      Response(res, { [this.controllerName]: data });
+    } catch (error) {
+      Response(res, { error: error }, status.INTERNAL_SERVER_ERROR);
+    }
+  };
+  post = async (req: express.Request, res: express.Response) => {
+    try {
+      const newData = new this.model(req.body);
+      await newData.save();
+      return Response(
+        res,
+        { message: 'Create completed', [this.controllerName]: newData },
+        status.CREATED
+      );
+    } catch (error) {
+      Response(res, { error: error }, status.INTERNAL_SERVER_ERROR);
+    }
+  };
+  deleteById = async (req: express.Request, res: express.Response) => {
+    try {
+      const { id } = req.params;
+      const data = await this.model.findByIdAndDelete(id).lean();
+      if (!data) {
+        return Response(
+          res,
+          {
+            message: `${this.controllerName} not found`
+          },
+          status.NOT_FOUND
+        );
+      }
+      Response(res, {
+        message: 'Delete completed',
+        [this.controllerName]: data
+      });
+    } catch (error) {
+      Response(res, { error: error }, status.INTERNAL_SERVER_ERROR);
+    }
+  };
+  update = async (req: express.Request, res: express.Response) => {
+    try {
+      const { id } = req.params;
+      const newData = await this.model
+        .findOneAndUpdate(
+          { _id: id },
+          {
+            $set: { ...req.body }
+          },
+          { new: true }
+        )
+        .lean();
+      if (!newData) {
+        return Response(
+          res,
+          { message: `${this.controllerName} not found` },
+          status.NOT_FOUND
+        );
+      }
+      return Response(
+        res,
+        { message: 'Edit completed', [this.controllerName]: newData },
+        status.OK
+      );
+    } catch (error) {
+      Response(res, { error: error }, status.INTERNAL_SERVER_ERROR);
+    }
+  };
+}
