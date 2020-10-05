d0fcb432f5ded584be874dbd72e179256a641d44
LQuangTien <tienkg5554@gmail.com>
2020-10-05 16:25:06 +0700

    allow create empty question

--- src/modules/Question/question.validate.ts
+++ src/modules/Question/question.validate.ts
@@ -1,16 +1,17 @@
 import Joi, { Schema } from 'joi';
 
 export const schema: Schema = Joi.object({
-  content: Joi.string(),
-  image: Joi.string(),
-  timeLimit: Joi.number(),
-  points: Joi.number(),
-  isSingleSelect: Joi.boolean(),
-  answers: Joi.object().keys({
-    A: Joi.alternatives().try(Joi.number(), Joi.string()),
-    B: Joi.alternatives().try(Joi.number(), Joi.string()),
-    C: Joi.alternatives().try(Joi.number(), Joi.string()),
-    D: Joi.alternatives().try(Joi.number(), Joi.string())
-  }),
-  correctAnswer: Joi.string()
+  content: Joi.string().allow(''),
+  image: Joi.string().allow(null),
+  timeLimit: Joi.number().allow(''),
+  points: Joi.number().allow(''),
+  answers: Joi.object()
+    .keys({
+      A: Joi.alternatives().try(Joi.number(), Joi.string()).allow(''),
+      B: Joi.alternatives().try(Joi.number(), Joi.string()).allow(''),
+      C: Joi.alternatives().try(Joi.number(), Joi.string()).allow(''),
+      D: Joi.alternatives().try(Joi.number(), Joi.string()).allow('')
+    })
+    .allow(''),
+  correctAnswer: Joi.string().allow('')
 });
