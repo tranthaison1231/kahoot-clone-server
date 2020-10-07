aa8a021f2bd6492ac9b650ffad66b6871e099001
LQuangTien <tienkg5554@gmail.com>
2020-10-07 08:26:48 +0700

    Multer store file in memoryStorage

---
+++ src/utils/uploadImage.ts
@@ -0,0 +1,10 @@
+import cloudinary from 'cloudinary';
+
+cloudinary.v2.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_KEY,
+  api_secret: process.env.CLOUD_SECRET
+});
+export const upload = (file: string) => {
+  return cloudinary.v2.uploader.upload(file);
+};
